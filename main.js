07c07a1ed11363c9b1e9b56491141f9bf7c0b6ab
Abubakrsiddik <vistulaabubakrsiddik@gmail.com>
2024-01-16 11:45:07 +0100

    third commit

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const menuBar = document.querySelector(".fa-bars");
 const close = document.querySelector(".fa-xmark");
 const sideBar = document.querySelector(".sidebar");
 
-menuBar.addEventListener('click', function () {
+
+menuBar.addEventListener('click', function() {
     sideBar.classList.add("open");
 });
 
-close.addEventListener('click', function () {
+
+close.addEventListener('click', function() {
     sideBar.classList.remove("open");
 });
 
+function reload() {
+    location.reload();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var navLinks = document.querySelectorAll('.items a');
+
+   
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+
+           
+            var targetSection = document.getElementById(targetId);
+
+           
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
 let cartItems = 0;
 let totalQuantity = 0;
 
@@ -21,7 +46,7 @@ function addToCart() {
 
 function updateBasketIcon() {
     const basketIcon = document.getElementById('basket-icon');
-    basketIcon.innerText = `🛒 ${cartItems} (${totalQuantity} items)`;
+    basketIcon.innerText = `${cartItems} (${totalQuantity} items)`;
 }
 
 function viewBasket() {
